6fcfe55974d4321364bee89784cad65f2d7f4a4e
MrWatMann <martin.prejsa1@gmail.com>
2020-02-28 14:58:13 +0100

    Initial commit

---
+++ Config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches {
+	class SlovakInsignias {
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		version = "1.0.0";
+		author[] = {"WattMann"};
+		authorUrl = "https://steamcommunity.com/id/ArchdukeWatt/";
+	};
+};
+
+class CfgUnitInsignia {
+	class Flag_insignia {
+		display_name = "Slovak Flag Insignia";
+		author = "Slovakia";
+		texture = "\SlovakInsignias\insignias\flag.paa";
+		textureVehicle = "";
+	};
+};
