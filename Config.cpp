368fee1b23a2de00b9bfbf3704d12d7a63f4ecdf
MrWatMann <martin.prejsa1@gmail.com>
2020-03-04 22:34:56 +0100

    Slovak Medical Service

--- Config.cpp
+++ Config.cpp
@@ -6,32 +6,28 @@ class CfgPatches {
 		requiredAddons[] = {};
 		version = "1.0.0";
 		author[] = {"WattMann", "DigitalRegent"};
-		authorUrl = "https://steamcommunity.com/id/ArchdukeWatt/";
+		authorUrl = "https://github.com/WattMann/SlovakInsigniaPack";
 	};
 
 };
 
 class CfgUnitInsignia {
-
 	class SlovakFlagInsignia {
 		display_name = "Slovak Flag Insignia";
 		author = "Slovak Insignia Pack";
 		texture = "\SlovakInsigniaPack\insignias\slovak_flag_insignia.paa";
 		textureVehicle = "";
 	};
-
-	class SlovakArmyInsignia {
-		display_name = "Army of the Slovak Republic"
+	class TankBattalionTrebisov {
+		display_name = "Tank Battalion Trebisov";
 		author = "Slovak Insignia Pack";
-		texture = "\SlovakInsigniaPack\insignias\slovak_army_insignia.paa"
+		texture = "\SlovakInsigniaPack\insignias\tpr_trebisov.paa";
 		textureVehicle = "";
 	};
-
-	class SlovakGeneralStaffInsignia {
-		display_name = "General Staff of the Slovak Republic"
-		author = "Slovak Insignia Pack"
-		texture = "\SlovakInsigniaPack\insignias\slovak_general_staff_insignia.paa"
-		textureVehicle = "";
+	class SlovakMedicalService {
+	    display_name = "slovak_medical_service.paa";
+	    author = "Slovak Insignia Pack";
+	    texture = "\SlovakInsigniaPack\insignias\slovak_medical_service.paa";
+	    textureVehicle = "";
 	};
-
 };
